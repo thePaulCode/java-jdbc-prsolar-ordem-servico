c0bde11beef2927340b89ede727c875530a4e85e
Paulo Santos <santospaulor21@gmail.com>
2023-12-29 11:56:11 -0300

    Adicionar arquivos ao .gitignore

--- src/main/java/dev/thepaulcode/model/GeradorMensagem.java
+++
@@ -1,9 +0,0 @@
-package dev.thepaulcode.model;
-
-public class GeradorMensagem {
-    OrdemServico ordemServico;
-
-//    public String mensagem(OrdemServico ordemServico){
-//        return System.out.println(ordemServico.cliente.getId(1).toString());
-//    }
-}
